21b9243e01d0ffebcebd97fc17c17d2e36990411
Joey Thomas <joeypunkrock11@gmail.com>
2018-12-13 15:30:00 +0000

    Added all files

---
+++ src/flash-message.js
@@ -0,0 +1,38 @@
+import toastr from 'toastr';
+
+(function($) {
+
+  'use strict';
+
+  const InnovedFlashMessage = function() {
+
+    /**
+     * Public methods
+     */
+    this.create = function(type, title, message, options) {
+      if(!message && !title) { return false; } //if no message and title given, exit now
+      type = type || 'info'; //set info to default type if none is set
+      options = options || ''; //set not extra options if not passed
+      toastr[type](message, title, options); //show message
+    };
+
+    this.init = function() {
+      toastr.options = {
+          "closeButton": true,
+          "positionClass": "toast-top-right",
+          "extendedTimeOut": "0"
+      }
+    };
+
+    return this;
+
+  };
+
+  $.innovedFlashMessage = function() {
+    const innovedFlashMessage = new InnovedFlashMessage();
+    return innovedFlashMessage;
+  }
+
+})(jQuery);
+
+export default $.innovedFlashMessage();
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const packageName = 'flash-message';
+
+module.exports = {
+  mode: "production",
+  entry: "./src/"+packageName+".js",
+  output: {
+    path: path.resolve(__dirname + "/dist"),
+    filename: packageName+".js",
+    publicPath: "/dist"
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ],
+      }]
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: packageName+".css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  externals: {
+    "jquery": "jQuery"
+  }
+};
